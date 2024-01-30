84537fc13a78497d675a20595a3e199610a3bfe9
Malthe <maaa0001@stud.kea.dk>
2024-01-30 14:29:48 +0100

    2

--- main.js
+++ main.js
@@ -4,5 +4,6 @@ setInterval(() => {
   all.forEach((bubble) => {
     bubble.style.transform = `translate(${Math.random() * 100}vw, ${Math.random() * 100}vh)`;
     bubble.style.background = `hsl(${Math.random() * 360}, 50%, 50%)`;
+    bubble.style.width = `${Math.random() * 100}px`;
   });
 }, 2000);
